adad6174167e58f663ba720828d946228bc625af
harshadasawant22@gmail.com <Panasonic@1209>
2023-04-25 12:07:52 +0530

    Role level Authorization

--- src/main/java/com/auth/controller/ProductController.java
+++ src/main/java/com/auth/controller/ProductController.java
@@ -1,14 +1,37 @@
 package com.auth.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.auth.dto.Product;
+import com.auth.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    ProductService service;
+
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to spring security app";
+    }
+//    @PostMapping("/new")
+//    public String addNewUser(@RequestBody UserInfo userInfo){
+//        return service.addUser(userInfo);
+//    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<Product> getAllTheProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
     }
 }
--- src/main/java/com/auth/demo/DemoApplication.java
+++ src/main/java/com/auth/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.auth.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.auth.*")
+@EnableMethodSecurity
 public class DemoApplication {
 
 	public static void main(String[] args) {
